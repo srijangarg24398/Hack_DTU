937ac9f2cd06d978c005a8d5ab44596393fe0b9c
vrs-git <vrs1124@gmail.com>
2017-10-14 05:26:46 +0530

    Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+
+  get '/home' => 'home#index'
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+ 
+
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
